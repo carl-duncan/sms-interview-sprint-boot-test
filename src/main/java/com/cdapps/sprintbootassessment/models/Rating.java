f6d0eb9a54ceed0c3269477f6a1287013ba4f956
Carl Duncan <carl.duncan@gkco.com>
2023-06-12 18:10:21 -0500

    feat: (models) #2 created the models with the getters and setters

---
+++ src/main/java/com/cdapps/sprintbootassessment/models/Rating.java
@@ -0,0 +1,31 @@
+package com.cdapps.sprintbootassessment.models;
+
+public class Rating {
+    private int movieId;
+    private double rating;
+    private int year;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/com/cdapps/sprintbootassessment/models/Star.java
@@ -0,0 +1,22 @@
+package com.cdapps.sprintbootassessment.models;
+
+public class Star {
+    private int movieId;
+    private int personId;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+}
