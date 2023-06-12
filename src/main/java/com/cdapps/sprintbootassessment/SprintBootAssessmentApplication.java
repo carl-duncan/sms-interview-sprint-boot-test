687741ff7b734d6720b142e3de309b6184241d02
Carl Duncan <carl.duncan@gkco.com>
2023-06-12 18:03:56 -0500

    first commit

---
+++ src/main/java/com/cdapps/sprintbootassessment/SprintBootAssessmentApplication.java
@@ -0,0 +1,11 @@
+package com.cdapps.sprintbootassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAssessmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootAssessmentApplication.class, args);
+    }
+}
